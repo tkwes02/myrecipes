c2d063488b652c3746feb61f468d8246701a9855
kuliodev <sobegogo@gmail.com>
2016-08-14 04:23:40 +0000

    remove production

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -9,7 +9,12 @@ process resize_to_limit: [600,600]
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
